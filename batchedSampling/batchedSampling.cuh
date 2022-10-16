eed416876cb0b77dc494f4d5e8cf730d7ede2f84
Adnan Jaljuli/aaj35@mail.aub.edu <aaj35@mail.aub.edu>
2022-10-16 11:34:07 +0300

    wrote initial sampling function

--- batchedSampling/batchedSampling.cuh
+++ batchedSampling/batchedSampling.cuh
@@ -1,17 +1,36 @@
 #ifndef __BATCHED_SAMPLING__
 #define __BATCHED_SAMPLING__
 
-// this function takes as input:
-// - an array of pointers to where each batch unit starts
-// - a scan ranks array
-// - an array of sampling vectors
-// - sampling vectors' width
-// - batch size
-// - unit size
-// - tile size
+static __global__ void batchedSampling(int tileSize, int batchSize, int batchUnitSize, double** U, double** V, int* scanRanks, double* samplingVectors, int samplingVectorsWidth, double* output, double* bufferMemory) {
+    unsigned int batch = blockIdx.x/batchSize;
+    unsigned int blockInBatch = blockIdx.x%batchSize;
+    int rank = (blockInBatch == 0) ? scanRanks[batch][tile*batchUnitSize + blockInBatch] : scanRanks[batch][tile*batchUnitSize + blockInBatch] - scanRanks[batch][tile*batchUnitSize + blockInBatch - 1];
+    output[batch*batchUnitSize*tileSize*samplingVectorsWidth + threadIdx.x*batchUnitSize*tileSize + blockInBatch*tileSize + threadIdx.y] = 0;
 
-static __global__ void batchedSampling(int tileSize, int batchSize, int unitSize, double** U, double** V, int* scanRanks, double* samplingVectors, int widthSamplingVector) {
-    
+    for(unsigned int tile = 0; tile < batchUnitSize; ++tile) {
+        // TODO: think about loading U, V and scanRanks into shared memory
+        // multiply V by the sampling vector and store it in buffer memory
+        int scanRanksIndex = (blockInBatch == 0) ? tile*batchUnitSize + blockInBatch : tile*batchUnitSize + blockInBatch - 1;
+        double sum = 0;
+
+        if(threadIdx.x < samplingVectorsWidth && threadIdx.y < rank) {
+            for(unsigned int j = 0; j < tileSize; ++j) {
+                sum += V[batch][scanRanks[batch][scanRanksIndex]*tileSize + threadIdx.x*tileSize + j]*samplingVectors[batch*batchUnitSize*tileSize*samplingVectorsWidth + threadIdx.x*batchUnitSize*tileSize + blockInBatch*tileSize + j];
+            }
+            bufferMemory[batch*batchUnitSize*tileSize*samplingVectorsWidth + threadIdx.x*batchUnitSize*tileSize + blockInBatch*tileSize + threadIdx.y] = sum;
+        }
+        __syncthreads();
+
+        // multiply U by the result and add it to output
+        sum = 0;
+        if(threadIdx.x < samplingVectorsWidth) {
+            for(unsigned int j = 0; j < rank; ++j) {
+                sum += U[batch][scanRanks[batch][scanRanksIndex]*tileSize + j*tileSize + threadIdx.y]*bufferMemory[batch*batchUnitSize*tileSize*samplingVectorsWidth + threadIdx.x*batchUnitSize*tileSize + blockInBatch*tileSize + j];
+            }
+            output[batch*batchUnitSize*tileSize*samplingVectorsWidth + threadIdx.x*batchUnitSize*tileSize + blockInBatch*tileSize + threadIdx.y] += sum;
+        }
+        __syncthreads();
+    }
 }
 
 #endif
--- batchedSampling/main.cu
+++ batchedSampling/main.cu
@@ -1,11 +1,14 @@
+#include "batchedSampling.cuh"
+
 #include <algorithm>
 #include <assert.h>
+#include <cub/cub.cuh>
+#include <curand_kernel.h>
 #include <inttypes.h>
 #include <iostream>
 #include <math.h>
 #include <string.h>
 #include <stdio.h>
-#include <curand_kernel.h>
 #include <time.h>
 #include <typeinfo>
 #include <utility>
@@ -22,10 +25,16 @@ __global__ void generateSamplingVectors(double *samplingVectors, int size) {
     }
 }
 
-int main() {
-    // TODO: read a batch of n*n TLR matrices from a file
-    fstream myFile("batchedMatrix.txt", ios_base::in);
+__global__ void fillBatchedPtrs(double** d_UBatchPtrs, double** d_VBatchPtrs, double* d_U, double* d_V, int* d_scanRanks, int batchSize, int segmentSize, int unitSize) {
+    for(int i = 0; i < batchSize; ++i) {
+        d_UBatchPtrs[i] = &d_U[d_scanRanks[i*unitSize*unitSize]*segmentSize];
+        d_VBatchPtrs[i] = &d_U[d_scanRanks[i*unitSize*unitSize]*segmentSize];
+    }
+}
 
+int main() {
+    // read a batch of n*n TLR matrices from a file
+    fstream myFile("batchedMatrix.txt", ios_base::in);
     int unitSize, segmentSize, batchSize;
     myFile >> unitSize >> segmentSize >> batchSize;
     printf("%d %d %d\n", unitSize, segmentSize, batchSize);
@@ -51,6 +60,8 @@ int main() {
             }
         }
     }
+
+    // TODO: make scanRanks a double pointer. make the scan of each batch unit independent of the other batches
     int *d_ranks, *d_scanRanks;
     double *d_U, *d_V;
     cudaMalloc((void**) &d_ranks, batchSize*unitSize*unitSize*sizeof(int));
@@ -63,27 +74,33 @@ int main() {
 
     void *d_temp_storage = NULL;
     size_t temp_storage_bytes = 0;
-    cub::DeviceScan::ExclusiveSum(d_temp_storage, temp_storage_bytes, d_ranks, d_scanRanks, batchSize*unitSize*unitSize);
+    cub::DeviceScan::InclusiveSum(d_temp_storage, temp_storage_bytes, d_ranks, d_scanRanks, batchSize*unitSize*unitSize);
     cudaMalloc(&d_temp_storage, temp_storage_bytes);
-    cub::DeviceScan::ExclusiveSum(d_temp_storage, temp_storage_bytes, d_ranks, d_scanRanks, batchSize*unitSize*unitSize);
+    cub::DeviceScan::InclusiveSum(d_temp_storage, temp_storage_bytes, d_ranks, d_scanRanks, batchSize*unitSize*unitSize);
 
     double **d_UBatchPtrs, **d_VBatchPtrs;
     cudaMalloc((void**) &d_UBatchPtrs, batchSize*sizeof(double*));
     cudaMalloc((void**) &d_VBatchPtrs, batchSize*sizeof(double*));
 
-    fillBatchedPtrs <<< numBlocks, numThreadsPerBlock >>> (d_UBatchPtrs, d_VBatchPtrs, d_U, d_V, batchSize, segmentSize, unitSize, rankSum);
+    unsigned int numBlocks = 1;
+    unsigned int numThreadsPerBlock = 1;
+    fillBatchedPtrs <<< numBlocks, numThreadsPerBlock >>> (d_UBatchPtrs, d_VBatchPtrs, d_U, d_V, d_scanRanks, batchSize, segmentSize, unitSize);
 
-    // TODO: generate random sampling vectors
+    // generate random sampling vectors
     unsigned int samplingVectorsWidth = 16;
+    double *d_output;
     double *d_samplingVectors;
+    cudaMalloc((void**) &d_output, samplingVectorsWidth*batchSize*segmentSize*unitSize*sizeof(double));
     cudaMalloc((void**) &d_samplingVectors, samplingVectorsWidth*batchSize*segmentSize*unitSize*sizeof(double));
-    unsigned int numThreadsPerBlock = 1024;
-    unsigned int numBlocks = (samplingVectorsWidth*batchSize*segmentSize*unitSize + numThreadsPerBlock - 1)/numThreadsPerBlock;
+    numThreadsPerBlock = 1024;
+    numBlocks = (samplingVectorsWidth*batchSize*segmentSize*unitSize + numThreadsPerBlock - 1)/numThreadsPerBlock;
     generateSamplingVectors <<< numBlocks, numThreadsPerBlock >>> (d_samplingVectors, samplingVectorsWidth*batchSize*segmentSize*unitSize);
 
-
-    // TODO: launch a kernel that takes as input the TLR matrices, sampling function and multiplies them and stores them in a matrix
-
+    // launch a kernel that takes as input the TLR matrices, sampling function and multiplies them and stores them in a matrix
+    numThreadsPerBlock = 1024;
+    numBlocks = batchSize*unitSize;
+    batchedSampling <<< numBlocks, numThreadsPerBlock >>> (segmentSize, batchSize, unitSize, d_UBatchPtrs, d_VBatchPtrs, d_scanRanks, d_samplingVectors, samplingVectorsWidth);
+    cudaDeviceSynchronize();
 
     // TODO: launch a kernel that checks the correctness of the multiplication
 }
