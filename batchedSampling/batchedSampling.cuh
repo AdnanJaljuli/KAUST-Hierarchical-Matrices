c850ee38fc64e84d99c875328d5551b387aa5ba8
Adnan Jaljuli/aaj35@mail.aub.edu <aaj35@mail.aub.edu>
2022-10-17 12:15:45 +0300

    added checking frobenius error

--- batchedSampling/batchedSampling.cuh
+++ batchedSampling/batchedSampling.cuh
@@ -3,20 +3,43 @@
 
 #include <bits/stdc++.h>
 
+static __host__ __device__ int getMOfromXY_h(unsigned int x, unsigned int y){
+    static const unsigned int B[] = {0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF};
+    static const unsigned int S[] = {1, 2, 4, 8};
+    x = (x | (x << S[3])) & B[3];
+    x = (x | (x << S[2])) & B[2];
+    x = (x | (x << S[1])) & B[1];
+    x = (x | (x << S[0])) & B[0];
+    y = (y | (y << S[3])) & B[3];
+    y = (y | (y << S[2])) & B[2];
+    y = (y | (y << S[1])) & B[1];
+    y = (y | (y << S[0])) & B[0];
+
+    int z = x | (y << 1);
+    return z;
+}
+
+static __host__ __device__ int IndextoMOIndex_h(int numSegments, int n){
+    unsigned int i = n%numSegments;
+    unsigned int j = n/numSegments;
+    return getMOfromXY_h(j, i);
+}
+
 static __global__ void batchedSampling(int tileSize, int batchSize, int batchUnitSize, double** U, double** V, int* scanRanks, double* samplingVectors, int samplingVectorsWidth, double* output, double* bufferMemory) {
     unsigned int batch = blockIdx.x/batchUnitSize;
     unsigned int blockInBatch = blockIdx.x%batchUnitSize;
     if(threadIdx.x < samplingVectorsWidth) {
-        output[batch*batchUnitSize*tileSize*samplingVectorsWidth + threadIdx.x*batchUnitSize*tileSize + blockInBatch*tileSize + threadIdx.y] = 0;
+        output[batch*batchUnitSize*tileSize*samplingVectorsWidth + threadIdx.x*batchUnitSize*tileSize + blockInBatch*tileSize + threadIdx.y] = 0.0f;
     }
     __syncthreads();
 
     for(unsigned int tile = 0; tile < batchUnitSize; ++tile) {
         // TODO: think about loading U, V and scanRanks into shared memory
         // multiply V by the sampling vector and store it in buffer memory
+        int MOIndex = IndextoMOIndex_h(batchUnitSize, tile*batchUnitSize + blockInBatch);
         double sum = 0;
-        int rank = (blockInBatch == 0 && tile == 0) ? scanRanks[batch*batchUnitSize*batchUnitSize] : scanRanks[batch*batchUnitSize*batchUnitSize + tile*batchUnitSize + blockInBatch] - scanRanks[batch*batchUnitSize*batchUnitSize + tile*batchUnitSize + blockInBatch - 1];
-        int scanRankVal = (blockInBatch == 0 && tile == 0) ? 0 : scanRanks[batch*batchUnitSize*batchUnitSize + tile*batchUnitSize + blockInBatch - 1];
+        int rank = (blockInBatch == 0 && tile == 0) ? (scanRanks[batch*batchUnitSize*batchUnitSize]) : (scanRanks[batch*batchUnitSize*batchUnitSize + MOIndex] - scanRanks[batch*batchUnitSize*batchUnitSize + MOIndex - 1]);
+        int scanRankVal = (blockInBatch == 0 && tile == 0) ? 0 : (scanRanks[batch*batchUnitSize*batchUnitSize + MOIndex - 1]);
 
         if(threadIdx.x < samplingVectorsWidth && threadIdx.y < rank) {
             for(unsigned int j = 0; j < tileSize; ++j) {
--- batchedSampling/main.cu
+++ batchedSampling/main.cu
@@ -54,6 +54,27 @@ __global__ void printOutput(double* output, int size) {
     }
 }
 
+__global__ void denseMatrixSampling(int batchSize, int matrixDim, double* denseMatrix, double* denseMatrixOutput, double* samplingVectors, int samplingVectorDim) {
+    // unsigned int batch = blockIdx.y/(matrixDim/32);
+    // unsigned int blockInBatch = blockIdx.y%(matrixDim/32);
+    if(threadIdx.x < samplingVectorDim) {
+        double sum = 0;
+        for(unsigned int i = 0; i < matrixDim; ++i) {
+            sum += denseMatrix[i*matrixDim + blockIdx.y*32 + threadIdx.y]*samplingVectors[threadIdx.x*matrixDim + i];
+        }
+        denseMatrixOutput[threadIdx.x*matrixDim + blockIdx.y*32 + threadIdx.y] = sum;
+    }
+}
+
+__global__ void compareResults(double* denseMatrixOutput, double* output, int size, double* error, double* tmp) {
+    for(unsigned int i = 0; i < size; ++ i) {
+        double x = denseMatrixOutput[i];
+        double y = output[i];
+        atomicAdd(tmp, x*x);
+        atomicAdd(error, (x - y)*(x - y));
+    }
+}
+
 int main() {
     // read a batch of n*n TLR matrices from a file
     fstream myFile("batchedMatrix.txt", ios_base::in);
@@ -105,7 +126,6 @@ int main() {
     cudaMalloc(&d_temp_storage, temp_storage_bytes);
     cub::DeviceScan::InclusiveSumByKey(d_temp_storage, temp_storage_bytes, d_batchSegments, d_ranks, d_scanRanks, batchSize*unitSize*unitSize);
 
-    printArray <<< 1, 1 >>> (d_scanRanks, batchSize*unitSize*unitSize);
     double **d_UBatchPtrs, **d_VBatchPtrs;
     cudaMalloc((void**) &d_UBatchPtrs, batchSize*sizeof(double*));
     cudaMalloc((void**) &d_VBatchPtrs, batchSize*sizeof(double*));
@@ -115,7 +135,7 @@ int main() {
     fillBatchedPtrs <<< numBlocks, numThreadsPerBlock >>> (d_UBatchPtrs, d_VBatchPtrs, d_U, d_V, d_scanRanks, batchSize, segmentSize, unitSize);
 
     // generate random sampling vectors
-    unsigned int samplingVectorsWidth = 16;
+    unsigned int samplingVectorsWidth = 32;
     double *d_output;
     double *d_bufferMemory;
     double *d_samplingVectors;
@@ -132,15 +152,41 @@ int main() {
     dim3 m_numBlocks(batchSize*unitSize, 1);
     batchedSampling <<< m_numBlocks, m_numThreadsPerBlock >>> (segmentSize, batchSize, unitSize, d_UBatchPtrs, d_VBatchPtrs, d_scanRanks, d_samplingVectors, samplingVectorsWidth, d_output, d_bufferMemory);
 
-    double* output = (double*)malloc(samplingVectorsWidth*batchSize*segmentSize*unitSize*sizeof(double));
-    cudaMemcpy(output, d_output, samplingVectorsWidth*batchSize*segmentSize*unitSize*sizeof(double), cudaMemcpyDeviceToHost);
-    char fileName[100] = "output.txt";
-    FILE *outputFile = fopen(fileName, "w");
-    for(int i = 0; i < samplingVectorsWidth*batchSize*segmentSize*unitSize; ++i) {
-        fprintf(outputFile, "%lf\n", output[i]);
+    // read the batched dense tiles form the txt file
+    fstream denseMatrixFile("denseMatrix.txt", ios_base::in);
+    double* denseMatrix = (double*)malloc(batchSize*unitSize*segmentSize*unitSize*segmentSize*sizeof(double));
+    for(unsigned int batch = 0; batch < batchSize; ++batch) {
+        for(unsigned int row = 0; row < unitSize*segmentSize; ++row) {
+            for(unsigned int col = 0; col < unitSize*segmentSize; ++col) {
+                denseMatrixFile >> denseMatrix[batch*unitSize*segmentSize*unitSize*segmentSize + col*unitSize*segmentSize + row];
+            }
+        }
     }
-    printf("done\n");
 
-    // TODO: launch a kernel that checks the correctness of the multiplication
-    
+    double* d_denseMatrix;
+    cudaMalloc((void**) &d_denseMatrix, batchSize*unitSize*segmentSize*unitSize*segmentSize*sizeof(double));    
+    cudaMemcpy(d_denseMatrix, denseMatrix, batchSize*unitSize*segmentSize*unitSize*segmentSize*sizeof(double), cudaMemcpyHostToDevice);
+    double *d_denseMatrixOutput;
+    cudaMalloc((void**) &d_denseMatrixOutput, samplingVectorsWidth*batchSize*segmentSize*unitSize*sizeof(double));
+
+    // multiply the dense matrix by the sampling vectors
+    dim3 dm_numThreadsPerBlock(samplingVectorsWidth, 32);
+    dim3 dm_numBlocks(1, (unitSize*segmentSize)/32);
+    denseMatrixSampling <<< dm_numBlocks, dm_numThreadsPerBlock >>> (batchSize, unitSize*segmentSize, d_denseMatrix, d_denseMatrixOutput, d_samplingVectors, samplingVectorsWidth);
+
+    // compare the results
+    double *d_error, *d_tmp;
+    cudaMalloc((void**) &d_error, sizeof(double));
+    cudaMalloc((void**) &d_tmp, sizeof(double));
+    cudaMemset(d_error, 0, sizeof(double));
+    cudaMemset(d_tmp, 0, sizeof(double));
+    compareResults <<< 1, 1 >>> (d_denseMatrixOutput, d_output, samplingVectorsWidth*batchSize*segmentSize*unitSize, d_error, d_tmp);
+    double h_error;
+    double h_tmp;
+    cudaMemcpy(&h_error, d_error, sizeof(double), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&h_tmp, d_tmp, sizeof(double), cudaMemcpyDeviceToHost);
+    printf("error in matrix: %lf\n", sqrt(h_error)/sqrt(h_tmp));
+    cudaFree(d_tmp);
+    cudaFree(d_error);
+    cudaDeviceSynchronize();
 }
